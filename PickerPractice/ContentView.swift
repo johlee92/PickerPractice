fee5d2e637d03c82a824f20cd908a1a9df7265e4
John <johnathanlee72@hotmail.com>
2022-04-18 16:50:51 -0700

    Init commit practice with pickers

--- PickerPractice/ContentView.swift
+++ PickerPractice/ContentView.swift
@@ -8,9 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var locationSelection = "Los Angeles"
+    @State var mealSelection = "Mac n Cheese"
+    @State var mealTime = Date()
+    
+    var locationList = ["San Francisco", "Los Angeles", "San Diego","Denver","New York City","Portland","Seatle","Boston"]
+    var mealList = ["Mac n Cheese","Pizza","Soup","Salad","Rice","Sushi","Sandwich","Coffee","Tea","Soda","Dessert"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Ottimo Ristorante")
+                .font(.largeTitle)
+            
+            HStack {
+                Text("Location:")
+                
+                Picker("Location", selection: $locationSelection) {
+                    ForEach(locationList, id: \.self) {
+                        loc in
+                        Text(loc).tag(loc)
+                    }
+                }.pickerStyle(.menu)
+                
+            }
+            
+            VStack {
+                Text("Order: ")
+                
+                Picker("Order", selection: $mealSelection) {
+                    ForEach(mealList, id: \.self) {
+                        meal in
+                        Text(meal).tag(meal)
+                    }
+                }.pickerStyle(.wheel)
+            }
+            
+            VStack {
+                Text("Pick Time")
+                
+                DatePicker(
+                    "",
+                    selection: $mealTime,
+                    displayedComponents: [.date]
+                )
+                .datePickerStyle(.wheel)
+            }
+            
+            Button {
+                locationSelection = locationList.randomElement()!
+                mealSelection = mealList.randomElement()!
+                mealTime = Date()
+                print("Location: \(locationSelection)")
+                print("Order: \(mealSelection)")
+                print("Time: \(mealTime)")
+            } label: {
+                Text("Pick for me")
+            }
+
+        }
     }
 }
 
