1b18fd3b0e3e9e3e0c1e01ab5bb0a8fb63cd9f8f
John <johnathanlee72@hotmail.com>
2022-04-18 16:31:09 -0700

    Initial Commit

---
+++ PickerPractice/PickerPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickerPracticeApp.swift
+//  PickerPractice
+//
+//  Created by Johnathan Lee on 4/18/22.
+//
+
+import SwiftUI
+
+@main
+struct PickerPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
